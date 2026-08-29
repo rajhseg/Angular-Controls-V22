bfb3f26a69c6f6206959901286ea0c32bf5ec878
Rajesh <rajhseg@gmail.com>
2026-08-29 15:40:09 +0530

    Update rnumeric.component.ts

--- Angular-Controls-V22/projects/rcomponents/src/lib/rnumeric/rnumeric.component.ts
+++ Angular-Controls-V22/projects/rcomponents/src/lib/rnumeric/rnumeric.component.ts
@@ -189,7 +189,7 @@ export class RNumericComponent extends RBaseComponent<number> implements Control
       this.NotifyToModel();
     } else {
       if(val > this.MaxValue) {
-        this.setMaxValue();
+        this.setAboveMaxValue(val);
       } else {
         this.setBelowMinValue(val);
       }  
@@ -204,12 +204,17 @@ export class RNumericComponent extends RBaseComponent<number> implements Control
     super(winObj);
   }
 
-  private setMaxValue(){
-    this._value = this.MaxValue;
-    this.backupValue = this._value;
-    this.ErrorMessage = '';
-    this.BottomLineColor = this.backupColor;
-    this.NotifyToModel();
+  private setAboveMaxValue(val: number){
+    
+    if(this.ErrorMessage == '') {
+      this.backupColor = this.BottomLineColor;
+      this.backupValue = this._value;
+    }
+
+    this._value = val;
+    
+    this.ErrorMessage = "Invalid max value";
+    this.BottomLineColor = this.ErrorIndicatorColor;  
   }
 
   private setMinValue(){
